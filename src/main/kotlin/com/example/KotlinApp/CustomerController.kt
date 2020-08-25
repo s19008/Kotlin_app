3aff787d6dbfbbbe77b6f3633e68fa2e67271655
Yo shinzato <s19008@std.it-college.ac.jp>
2020-08-25 11:54:30 +0900

    first commit

--- src/main/kotlin/com/example/KotlinApp/CustomerController.kt
+++ src/main/kotlin/com/example/KotlinApp/CustomerController.kt
@@ -3,11 +3,15 @@ package com.example.KotlinApp
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class HomeController {
+@RequestMapping("customers")
+class CustomerController {
     @GetMapping("")
     fun index(model: Model): String {
-        return "home"
+        model.addAttribute("customers", "")
+                return "customers/index"
     }
+
 }
